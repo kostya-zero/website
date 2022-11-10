e598cf03b7b091a76bba745bf780eadb846c7cba
Konstantin Zhigaylo <kostyantop8@gmail.com>
2022-11-10 16:36:45 +0300

    Initial Commit

---
+++ functions.js
@@ -0,0 +1,15 @@
+function go_to_workspace() {
+    window.location.href = "workspace.html";
+}
+
+function go_to_home() {
+    window.location.href = "index.html";
+}
+
+function open_github() {
+    window.open("https://github.com/kostya-zero", "_blank").focus()
+}
+
+function open_gitlab() {
+    window.open("https://gitlab.com/kostya-zero", "_blank").focus()
+}
