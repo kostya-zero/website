2c5aac23c65381fee405bd71bda256997992c056
Konstantin Zhigaylo <kostyantop8@gmail.com>
2022-11-10 17:15:58 +0300

    Add links to footer

--- functions.js
+++ functions.js
@@ -13,3 +13,15 @@ function open_github() {
 function open_gitlab() {
     window.open("https://gitlab.com/kostya-zero", "_blank").focus()
 }
+
+function open_mast() {
+    window.open("https://mastodon.ml/@kostya_zer0", "_blank").focus()
+}
+
+function open_matrix() {
+    window.open("https://matrix.to/#/@kostya_zer0:matrix.org", "_blank").focus()
+}
+
+function open_tg() {
+    window.open("https://t.me/kostya_zer0", "_blank").focus()
+}
